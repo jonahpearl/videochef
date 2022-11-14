7b56ca3050ae4bce88b8cec50be79b16366f48a3
jonahpearl <j4jonahp@gmail.com>
2022-11-13 22:12:34 -0500

    various utils

---
+++ src/videochef/util.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def gen_batch_sequence(nframes, chunk_size, overlap, offset=0):
+    '''
+    Generates batches used to chunk videos prior to extraction.
+    Parameters
+    ----------
+    nframes (int): total number of frames
+    chunk_size (int): desired chunk size
+    overlap (int): number of overlapping frames
+    offset (int): frame offset
+    Returns
+    -------
+    Yields list of batches
+    '''
+
+    seq = range(offset, nframes)
+    out = []
+    for i in range(0, len(seq) - overlap, chunk_size - overlap):
+        out.append(seq[i:i + chunk_size])
+    return out
+
+
+def dummy_process(frame):
+    # Normalize the frame's pixel vals around half max brightness (just something stupid that isn't instant)
+    avg = np.mean(frame)
+    std = np.std(frame)
+    frame = ((frame - avg)/std + 255/2).astype('uint8')
+    return frame
+
+
+def count_frames(file_name):
+    with av.open(file_name, 'r') as reader:
+        return reader.streams.video[0].frames          
