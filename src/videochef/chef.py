fb7a655185c55f275e4e4a5b8d168501d2af9382
jonahpearl <j4jonahp@gmail.com>
2022-11-13 22:13:36 -0500

    fix imports to not interfere with base io

--- src/videochef/chef.py
+++ src/videochef/chef.py
@@ -1,20 +1,8 @@
 import numpy as np
-import av
-from os.path import join, exists
-
-import time
-import cv2
-from tqdm import tqdm
-from tqdm.contrib.concurrent import process_map
-from imageio import get_reader
-from itertools import repeat, count
-import src.videochef.io as io
-
-import imageio as iio
-import pdb
+from videochef.io import videoReader, videoWriter
 
 
-def parallel_proc_frame(vid_path, frame_batch, reporter_val, writer_path, analysis_func=process_frame):
-    with io.videoReader(vid_path, np.array(frame_batch), reporter_val) as vid, io.videoWriter(writer_path) as writer:
+def parallel_proc_frame(vid_path, frame_batch, reporter_val, writer_path, analysis_func=None):
+    with videoReader(vid_path, np.array(frame_batch), reporter_val) as vid, videoWriter(writer_path) as writer:
         for frame in vid:
             writer.append(analysis_func(frame))
