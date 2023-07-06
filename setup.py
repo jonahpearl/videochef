6c16847850a76ac5f3f5862fa1879f0c6230f7e6
jonahpearl <68478436+jonahpearl@users.noreply.github.com>
2023-07-06 00:28:12 -0400

    Bug fixes

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 
 setup(
     name='videochef',
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
     description='Whip up some fast, embarrassingly parallel video processing',
     author='Jonah Pearl',
