8fbbeb03587243b267833e52ee6548d8347b94fd
jonahpearl <j4jonahp@gmail.com>
2024-02-08 00:36:01 -0500

    black formatting

--- src/videochef/util.py
+++ src/videochef/util.py
@@ -1,8 +1,9 @@
 import numpy as np
 import av
 
+
 def make_batch_sequence(nframes, chunk_size, overlap, offset=0, step=1):
-    '''
+    """
     Generates batches used to chunk videos prior to extraction.
     Parameters
     ----------
@@ -13,44 +14,48 @@ def make_batch_sequence(nframes, chunk_size, overlap, offset=0, step=1):
     Returns
     -------
     Returns list of batches
-    '''
+    """
 
     if overlap >= chunk_size:
-        raise ValueError('Overlap must be less than chunk size')
-    
+        raise ValueError("Overlap must be less than chunk size")
+
     seq = range(offset, nframes, step)
     out = []
     for i in range(0, len(seq) - overlap, chunk_size - overlap):
-        out.append(seq[i:i + chunk_size])
+        out.append(seq[i : i + chunk_size])
     return out
 
 
 def unwrap_dictionary(dict_):
     """Take a dictionary of form {'a': vals, 'b': vals, ...} and convert to a list of form [{'a': val1, 'b': val2, ...}, {'a': val2, 'b': val2, ...}, ...]
         Vals must all be same length!
-        
+
     Arguments:
         dict_ {[type]} -- [description]
     """
 
     # Transform kwarg dict from {'a': vals, 'b': vals, ...} into [{'a': val1, 'b': val1,...}, {'a': val2, 'b': val2,...}, ...]
-    tmp_lists = []  # Intermdiate step: first unpack each value for each dict [[{'a':val1, 'a': val2}], [{'b': val1}, {'b':val2}], ...]
+    tmp_lists = (
+        []
+    )  # Intermdiate step: first unpack each value for each dict [[{'a':val1, 'a': val2}], [{'b': val1}, {'b':val2}], ...]
     for key in dict_.keys():
-        tmp_lists.append([{key:v} for v in dict_[key]])
-    
+        tmp_lists.append([{key: v} for v in dict_[key]])
+
     # Then put into desired final form
     final_list = []
     for dicts in zip(*tmp_lists):
-        final_list.append({k:v for d in dicts for k,v in d.items()})
+        final_list.append({k: v for d in dicts for k, v in d.items()})
 
     return final_list
+
 
 def dummy_process(frame):
     # Normalize the frame's pixel vals around half max brightness (just something stupid that isn't instant)
     avg = np.mean(frame)
     std = np.std(frame)
-    frame = ((frame - avg)/std + 255/2).astype('uint8')
+    frame = ((frame - avg) / std + 255 / 2).astype("uint8")
     return (frame,)
+
 
 def dummy_process_arrays(frame):
     # Get the min, max, and mean of each frame
@@ -58,37 +63,37 @@ def dummy_process_arrays(frame):
     avg = np.mean(frame)
     _min = np.min(frame)
     _max = np.max(frame)
-    midx = frame.shape[0]//2
-    midy = frame.shape[1]//2
-    four_middle_px = frame[midx-1:midx+1, midy-1:midy+1]
-    
+    midx = frame.shape[0] // 2
+    midy = frame.shape[1] // 2
+    four_middle_px = frame[midx - 1 : midx + 1, midy - 1 : midy + 1]
+
     # return a dictionary with the results in it
-    out = {'avg': avg, 'min': _min, 'max': _max, 'four_middle_px': four_middle_px}
-    
+    out = {"avg": avg, "min": _min, "max": _max, "four_middle_px": four_middle_px}
+
     return (out,)
 
+
 def dummy_process_vid_and_arrays(frame):
-    """ Return (frame, out_dict)
-    """
-    
+    """Return (frame, out_dict)"""
+
     # Normalize the frame's pixel vals around half max brightness (just something stupid that isn't instant)
     avg = np.mean(frame)
     std = np.std(frame)
-    frame = ((frame - avg)/std + 255/2).astype('uint8')
+    frame = ((frame - avg) / std + 255 / 2).astype("uint8")
 
     # Get the min, max, and mean of each frame
     # Also get the 4 middle pixels, just to test the ability to store >1-D arrays
     avg = np.mean(frame)
     _min = np.min(frame)
     _max = np.max(frame)
-    midx = frame.shape[0]//2
-    midy = frame.shape[1]//2
-    four_middle_px = frame[midx-1:midx+1, midy-1:midy+1]
-    out_dict = {'avg': avg, 'min': _min, 'max': _max, 'four_middle_px': four_middle_px}
-    
+    midx = frame.shape[0] // 2
+    midy = frame.shape[1] // 2
+    four_middle_px = frame[midx - 1 : midx + 1, midy - 1 : midy + 1]
+    out_dict = {"avg": avg, "min": _min, "max": _max, "four_middle_px": four_middle_px}
+
     return (frame, out_dict)
 
 
 def count_frames(file_name):
-    with av.open(file_name, 'r') as reader:
-        return reader.streams.video[0].frames          
+    with av.open(file_name, "r") as reader:
+        return reader.streams.video[0].frames
--- src/videochef/viz.py
+++ src/videochef/viz.py
@@ -9,12 +9,9 @@ from os import listdir, makedirs, rmdir, remove
 
 import pdb
 
+
 def add_titles(
-    panels, 
-    titles, 
-    text_origin=(5,18), 
-    text_size=0.4, 
-    text_color=(255,255,255)
+    panels, titles, text_origin=(5, 18), text_size=0.4, text_color=(255, 255, 255)
 ):
     """Helper function to add titles to a set of video panels
 
@@ -30,20 +27,21 @@ def add_titles(
     Returns:
         panels -- panels with titles appended
     """
-    
+
     return [
         cv2.putText(
             panel.copy(),
-            title, 
-            text_origin, 
-            cv2.FONT_HERSHEY_SIMPLEX, 
-            text_size, 
-            text_color, 
-            1, 
-            cv2.LINE_AA
-        ) 
-        for panel,title in zip(panels,titles)
+            title,
+            text_origin,
+            cv2.FONT_HERSHEY_SIMPLEX,
+            text_size,
+            text_color,
+            1,
+            cv2.LINE_AA,
+        )
+        for panel, title in zip(panels, titles)
     ]
+
 
 def peri_event_vid(
     in_vid_name,
@@ -76,7 +74,7 @@ def peri_event_vid(
         stderrfile {str} -- path to send ffmpeg stderr to, if any. Can't capture stderr programatically due to how we write the frames one at a time.
         crf {int} -- quality factor. Normal default is 23, here we boost it a bit (lower is better), because science. (default: {10})
         ffmpeg_loglevel {str} -- loglevel for ffmpeg. Useful if you get weird bugs and want to debug.
-        
+
     Returns: n/a
 
     Dummy example:
@@ -99,7 +97,7 @@ def peri_event_vid(
             )
         except BrokenPipeError:
             warnings.warn('Encountered broken pipe, skipping...')
-        
+
     """
 
     if traces is not None:
@@ -107,47 +105,52 @@ def peri_event_vid(
 
     # Plan layout to be as close to square as possible
     num_stims = len(peri_evt_frames_list)
-    nrows = np.ceil(np.sqrt(num_stims)).astype('int')
+    nrows = np.ceil(np.sqrt(num_stims)).astype("int")
 
     # Skip already existing vids
-    if exists(out_vid_name) and not overwrite: 
+    if exists(out_vid_name) and not overwrite:
         return None
 
     # libx264 is a better verison of h264.
     # yuv420 is open-able by quicktime and most video players.
     # NB, user should still pass in RGB frames -- it will be converted by ffmpeg into yuv (see videochef.io.write_frames()).
     waiting_writer = VideoWriter(
-        out_vid_name, 
-        fps=out_fps, 
-        codec='libx264', 
-        pixel_format='yuv420p',  
+        out_vid_name,
+        fps=out_fps,
+        codec="libx264",
+        pixel_format="yuv420p",
         verbose=verbose,
         **write_frame_kwargs,
     )
     with ExitStack() as stack:
 
         # Get video readers (and they seek to exactly the right place on this line!)
-        if verbose: print('Getting readers...')
+        if verbose:
+            print("Getting readers...")
         peristim_readers = [
             stack.enter_context(
                 VideoReader(
-                    in_vid_name, 
+                    in_vid_name,
                     np.array(frames),
                 )
-            ) for frames in peri_evt_frames_list
+            )
+            for frames in peri_evt_frames_list
         ]
 
-        if verbose: print('getting writer')
+        if verbose:
+            print("getting writer")
         writer = stack.enter_context(waiting_writer)
 
         # Iterate through all readers in parallel ((frame0 stim0, frame0 stim1,...), (frame1 stim0, frame1 stim1,...), ...)
         # which will get condensed across stims into bigframe 0, bigframe 1, bigframe 2,...
         # ix counts aligned frame num (ie starts at 0)
-        for ix, frames in tqdm(enumerate(zip(*peristim_readers)), total=len(peri_evt_frames_list[0])):
+        for ix, frames in tqdm(
+            enumerate(zip(*peristim_readers)), total=len(peri_evt_frames_list[0])
+        ):
             all_rows = []
 
             # Add each mini-frame to the big frame
-            panels,titles = [],[]  # empty lists for frame
+            panels, titles = [], []  # empty lists for frame
 
             ii = 0  # counter for layout
             for iReader, frame in enumerate(frames):
@@ -156,60 +159,70 @@ def peri_event_vid(
                 if frame.ndim == 2:
                     frame = np.stack(3 * (frame,), axis=-1)
                 elif frame.ndim != 3:
-                    raise ValueError('Expected 3D frames but got frames of shape {frame.shape}')
+                    raise ValueError(
+                        "Expected 3D frames but got frames of shape {frame.shape}"
+                    )
 
                 # Append a mark for whether event fr has passed or not
                 if event_frame_num_in_vid is not None:
-                    if (ix < event_frame_num_in_vid):
+                    if ix < event_frame_num_in_vid:
                         fill = 1
                     else:
                         fill = -1  # for some reason, -1 fills it
-                    frame = cv2.circle(frame, (18,96), 15, (0,255,0), fill)
+                    frame = cv2.circle(frame, (18, 96), 15, (0, 255, 0), fill)
 
                 # Mark traces if requested
                 # TODO: have to mark real-time on the trace by tracking the right xval. Boundary conditions make it hard. pure midpoint only works when neither boundary is limiting.
-#                 trace_start_idx = int(max(0, (ix*trace_to_vid_fs_ratio - 200)))
-#                 trace_end_idx = int(min(traces.shape[1], (ix*trace_to_vid_fs_ratio + 200)))
-#                 slice_to_show = slice(trace_start_idx, trace_end_idx, 2)
-#                 if traces is not None:
-#                     vals = traces[iReader, slice_to_show]
-#                     vals_on_frame_x = np.arange(len(vals))
-#                     vals_on_frame_y = 150 + -1*vals*20
-#                     pts = np.column_stack([vals_on_frame_x, vals_on_frame_y]).round().astype(np.int32)
-#                     frame = cv2.polylines(frame, [pts], False, (0,255,0))
-#                     midpoint_idx = len(pts)//2
-#                     frame = cv2.circle(frame, pts[midpoint_idx], 3, (255,0,0), fill)
-    
+                #                 trace_start_idx = int(max(0, (ix*trace_to_vid_fs_ratio - 200)))
+                #                 trace_end_idx = int(min(traces.shape[1], (ix*trace_to_vid_fs_ratio + 200)))
+                #                 slice_to_show = slice(trace_start_idx, trace_end_idx, 2)
+                #                 if traces is not None:
+                #                     vals = traces[iReader, slice_to_show]
+                #                     vals_on_frame_x = np.arange(len(vals))
+                #                     vals_on_frame_y = 150 + -1*vals*20
+                #                     pts = np.column_stack([vals_on_frame_x, vals_on_frame_y]).round().astype(np.int32)
+                #                     frame = cv2.polylines(frame, [pts], False, (0,255,0))
+                #                     midpoint_idx = len(pts)//2
+                #                     frame = cv2.circle(frame, pts[midpoint_idx], 3, (255,0,0), fill)
+
                 # Add to frame
-                panels.append(frame.astype('uint8'))
-                titles.append(f'Instance: {iReader}; fr: {ix}')
+                panels.append(frame.astype("uint8"))
+                titles.append(f"Instance: {iReader}; fr: {ix}")
 
                 # If at end of row, or end of rectangle, fill out big frame
-                if ii == (nrows-1):
-                    panels = add_titles(panels, titles, text_size=1, text_origin=(18,32))
-#                     panels = add_titles(panels, odor_infos, text_size=1, text_origin=(18,64))
+                if ii == (nrows - 1):
+                    panels = add_titles(
+                        panels, titles, text_size=1, text_origin=(18, 32)
+                    )
+                    #                     panels = add_titles(panels, odor_infos, text_size=1, text_origin=(18,64))
                     all_rows.append(panels)
                     ii = 0
-                    panels,titles = [],[]
-                elif iReader == (len(frames)-1):
-                    panels = add_titles(panels, titles, text_size=1, text_origin=(18,32))
+                    panels, titles = [], []
+                elif iReader == (len(frames) - 1):
+                    panels = add_titles(
+                        panels, titles, text_size=1, text_origin=(18, 32)
+                    )
                     all_rows.append(panels)
                     ii = 0
                 else:
                     ii += 1
-            
+
             # Stack the individual panels into rows
             stacked_rows = [np.hstack(panels) for panels in all_rows]
-            
+
             # Deal with any non-full rows
             max_row = np.argmax([row.size for row in stacked_rows])
             for iRow in range(len(stacked_rows)):
                 diff = stacked_rows[max_row].shape[1] - stacked_rows[iRow].shape[1]
                 if diff != 0:
-                    stacked_rows[iRow] = np.pad(stacked_rows[iRow], ((0,0), (0,diff), (0,0)), constant_values=0)
-            
+                    stacked_rows[iRow] = np.pad(
+                        stacked_rows[iRow],
+                        ((0, 0), (0, diff), (0, 0)),
+                        constant_values=0,
+                    )
+
             # Stack the rows (now all identical shapes)
-            big_frame = np.vstack(stacked_rows).astype('uint8')
+            big_frame = np.vstack(stacked_rows).astype("uint8")
 
             # Height and width must be divisible by two, at least for libx264
             for dim in range(big_frame.ndim):
@@ -217,12 +230,13 @@ def peri_event_vid(
                 if (dim == 0 or dim == 1) and big_shape[dim] % 2 == 1:
                     zeros_shape = list(big_shape)
                     zeros_shape[dim] = 1
-                    big_frame = np.concatenate([big_frame, np.zeros(zeros_shape)], axis=dim)
+                    big_frame = np.concatenate(
+                        [big_frame, np.zeros(zeros_shape)], axis=dim
+                    )
 
-            
-            if verbose: 
-                print(f'Writing frame {ix}...')
-                print(f'Frame shape: {big_frame.shape}')
+            if verbose:
+                print(f"Writing frame {ix}...")
+                print(f"Frame shape: {big_frame.shape}")
             writer.append(big_frame)
 
     return
